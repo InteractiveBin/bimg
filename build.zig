cd667d16a1dd8b22c0236291bcc3c3eec25499d4
Leonardo Gatti <leonardogattidev@gmail.com>
2025-01-17 02:39:33 -0300

    updated to zig master

--- build.zig
+++ build.zig
@@ -4,25 +4,26 @@ pub fn build(b: *std.Build) void {
     const target = b.standardTargetOptions(.{});
     const optimize = b.standardOptimizeOption(.{});
 
-    const bx_lib = b.dependency("bx", .{ .target = target, .optimize = optimize }).artifact("bx");
+    const bx_lib = b.lazyDependency("bx", .{ .target = target, .optimize = optimize }).?.artifact("bx");
     const bimg_lib = b.addStaticLibrary(.{
         .name = "bimg",
         .target = target,
         .optimize = optimize,
     });
-    bimg_lib.defineCMacro("BX_CONFIG_DEBUG", "0"); // Release
-    bimg_lib.addCSourceFiles(.{ .files = &src_files });
-    bimg_lib.linkLibCpp();
-    bimg_lib.addIncludePath(b.path("include"));
-    bimg_lib.addIncludePath(b.path("3rdparty"));
-    bimg_lib.addIncludePath(b.path("3rdparty/astc-encoder/include"));
-    bimg_lib.addIncludePath(b.path("3rdparty/iqa/include"));
-    bimg_lib.addIncludePath(b.path("3rdparty/tinyexr/deps/miniz"));
+    const bimg_mod = bimg_lib.root_module;
+    bimg_mod.addCSourceFiles(.{ .files = &src_files });
+    bimg_mod.link_libcpp = true;
+    bimg_mod.addCMacro("BX_CONFIG_DEBUG", "0"); // Release
+    bimg_mod.addIncludePath(b.path("include"));
+    bimg_mod.addIncludePath(b.path("3rdparty"));
+    bimg_mod.addIncludePath(b.path("3rdparty/astc-encoder/include"));
+    bimg_mod.addIncludePath(b.path("3rdparty/iqa/include"));
+    bimg_mod.addIncludePath(b.path("3rdparty/tinyexr/deps/miniz"));
+    bimg_mod.linkLibrary(bx_lib);
+
     bimg_lib.installHeadersDirectory(b.path("include"), ".", .{
         .include_extensions = &.{ ".h", ".inl" },
     });
-    bimg_lib.linkLibrary(bx_lib);
-
     b.installArtifact(bimg_lib);
 }
 
