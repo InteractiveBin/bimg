a397e36c843f0edd2539973bf628d61653bd521e
Leonardo Gatti <leonardogattidev@gmail.com>
2025-01-20 05:24:30 -0300

    fix

--- build.zig
+++ build.zig
@@ -1,36 +1,98 @@
 const std = @import("std");
 
-pub fn build(b: *std.Build) void {
+pub fn build(b: *std.Build) !void {
     const target = b.standardTargetOptions(.{});
     const optimize = b.standardOptimizeOption(.{});
 
-    const bx_lib = b.lazyDependency("bx", .{ .target = target, .optimize = optimize }).?.artifact("bx");
-    const bimg_lib = b.addStaticLibrary(.{
-        .name = "bimg",
+    const mod = b.addModule("bimg", .{
         .target = target,
         .optimize = optimize,
     });
-    const bimg_mod = bimg_lib.root_module;
-    bimg_mod.addCSourceFiles(.{ .files = &src_files });
-    bimg_mod.link_libcpp = true;
-    bimg_mod.addCMacro("BX_CONFIG_DEBUG", "0"); // Release
-    bimg_mod.addIncludePath(b.path("include"));
-    bimg_mod.addIncludePath(b.path("3rdparty"));
-    bimg_mod.addIncludePath(b.path("3rdparty/astc-encoder/include"));
-    bimg_mod.addIncludePath(b.path("3rdparty/iqa/include"));
-    bimg_mod.addIncludePath(b.path("3rdparty/tinyexr/deps/miniz"));
-    bimg_mod.linkLibrary(bx_lib);
+    const lib = b.addStaticLibrary(.{
+        .name = "bimg",
+        .root_module = mod,
+    });
 
-    bimg_lib.installHeadersDirectory(b.path("include"), ".", .{
+    const t = target.result;
+    const is_debug = mod.optimize == .Debug;
+
+    mod.addCSourceFiles(.{
+        .flags = &cpp_flags,
+        .files = &cpp_src,
+    });
+    mod.link_libcpp = true;
+
+    mod.addCMacro("__STDC_LIMIT_MACROS", "");
+    mod.addCMacro("__STDC_FORMAT_MACROS", "");
+    mod.addCMacro("__STDC_CONSTANT_MACROS", "");
+    mod.addCMacro(if (is_debug) "_DEBUG" else "NDEBUG", "");
+    mod.addCMacro("BX_CONFIG_DEBUG", if (is_debug) "1" else "0");
+
+    mod.addIncludePath(b.path("include"));
+    mod.addIncludePath(b.path("3rdparty/astc-encoder/include"));
+    mod.addIncludePath(b.path("3rdparty"));
+    mod.addIncludePath(b.path("3rdparty/tinyexr/deps/miniz"));
+    switch (t.os.tag) {
+        .windows => {
+            mod.addCMacro("WIN32", "1");
+            switch (t.abi) {
+                .gnu => {
+                    mod.addCMacro("MINGW_HAS_SECURE_API", "1");
+                },
+                else => {},
+            }
+        },
+        else => {},
+    }
+
+    for (deps) |dep|
+        mod.linkLibrary(b.dependency(dep, .{}).artifact(dep));
+
+    lib.installHeadersDirectory(b.path("include"), ".", .{
         .include_extensions = &.{ ".h", ".inl" },
     });
-    b.installArtifact(bimg_lib);
+    b.installArtifact(lib);
 }
 
-const src_files = [_][]const u8{
-    "src/image_decode.cpp",
+const common_flags = [_][]const u8{
+    "-fno-sanitize=undefined",
+};
+const c_flags = common_flags;
+const cpp_flags = [_][]const u8{
+    "-std=c++17",
+} ++ common_flags;
+
+const cpp_src = [_][]const u8{
+    "3rdparty/astc-encoder/source/astcenc_averages_and_directions.cpp",
+    "3rdparty/astc-encoder/source/astcenc_block_sizes.cpp",
+    "3rdparty/astc-encoder/source/astcenc_color_quantize.cpp",
+    "3rdparty/astc-encoder/source/astcenc_color_unquantize.cpp",
+    "3rdparty/astc-encoder/source/astcenc_compress_symbolic.cpp",
+    "3rdparty/astc-encoder/source/astcenc_compute_variance.cpp",
+    "3rdparty/astc-encoder/source/astcenc_decompress_symbolic.cpp",
+    "3rdparty/astc-encoder/source/astcenc_diagnostic_trace.cpp",
+    "3rdparty/astc-encoder/source/astcenc_entry.cpp",
+    "3rdparty/astc-encoder/source/astcenc_find_best_partitioning.cpp",
+    "3rdparty/astc-encoder/source/astcenc_ideal_endpoints_and_weights.cpp",
+    "3rdparty/astc-encoder/source/astcenc_image.cpp",
+    "3rdparty/astc-encoder/source/astcenc_integer_sequence.cpp",
+    "3rdparty/astc-encoder/source/astcenc_mathlib.cpp",
+    "3rdparty/astc-encoder/source/astcenc_mathlib_softfloat.cpp",
+    "3rdparty/astc-encoder/source/astcenc_partition_tables.cpp",
+    "3rdparty/astc-encoder/source/astcenc_percentile_tables.cpp",
+    "3rdparty/astc-encoder/source/astcenc_pick_best_endpoint_format.cpp",
+    "3rdparty/astc-encoder/source/astcenc_quantization.cpp",
+    "3rdparty/astc-encoder/source/astcenc_symbolic_physical.cpp",
+    "3rdparty/astc-encoder/source/astcenc_weight_align.cpp",
+    "3rdparty/astc-encoder/source/astcenc_weight_quant_xfer_tables.cpp",
     "src/image.cpp",
-    "src/image_encode.cpp",
     "src/image_gnf.cpp",
-    "src/image_cubemap_filter.cpp",
+    "src/image_decode.cpp",
+};
+const c_src = [_][]const u8{
+    "3rdparty/tinyexr/deps/miniz/miniz.c",
+};
+
+const deps = [_][]const u8{
+    "bx",
 };
